f1974d565d29a4cee00b14fb16bbbacbaca19ca8
pnguyen <9phuc.nguyen6@gmail.com>
2023-03-04 14:25:55 -0600

    testing input pullup

--- musik.ino
+++ musik.ino
@@ -1,35 +1,41 @@
 #include "SoftwareSerial.h"
 #include "DFRobotDFPlayerMini.h"
 
-static const uint8_t PIN_DFM_TX   = 2;
 static const uint8_t PIN_DFM_RX = 3;
-static const uint8_t PIN_ = 3;
-static const uint8_t PIN_MP3_RX = 3;
-SoftwareSerial softwareSerial(PIN_MP3_RX, PIN_MP3_TX);
+static const uint8_t PIN_DFM_TX = 2;
+static const uint8_t PIN_TOGGLE_CONSTANT = 12;
+static const uint8_t PIN_TOGGLE_RANDOM = 13;
 
-//   Create the Player object
+SoftwareSerial softwareSerial(PIN_DFM_RX, PIN_DFM_TX);
 DFRobotDFPlayerMini player;
 
 void setup() {
-
-  // Init USB serial port for debugging
   Serial.begin(9600);
-  // Init serial   port for DFPlayer Mini
   softwareSerial.begin(9600);
 
-  if
-  // Start communication   with DFPlayer Mini
-  if (player.begin(softwareSerial)) {
-      Serial.println("OK");
+  pinMode(PIN_TOGGLE_CONSTANT, INPUT_PULLUP);
+  pinMode(PIN_TOGGLE_RANDOM, INPUT_PULLUP);
 
-      // Set volume to maximum (0 to 30).
-      player.volume(30);
-      // Play   the first MP3 file on the SD card
-      player.play(1);
-    } else {
-      Serial.println("Connecting   to DFPlayer Mini failed!");
-    }
+  if (player.begin(softwareSerial)) {
+    Serial.println("Initialized DFPlayer Mini");
+
+    // Set volume to maximum (0 to 30).
+    player.volume(30);
+    // Play   the first MP3 file on the SD card
+    player.play(1);
+  } else {
+    Serial.println("Connecting   to DFPlayer Mini failed!");
+  }
 }
 
 void loop() {
+  if (digitalRead(PIN_TOGGLE_CONSTANT) == HIGH) {
+      Serial.println("CONST HIGH");
+    }
+
+    if (digitalRead(PIN_TOGGLE_RANDOM) == HIGH) {
+      Serial.println("RAND HIGH");
+    }
+
+    
 }
