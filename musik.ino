88064767c4723fe9b13314f51f7eb579b9e801a8
pnguyen <9phuc.nguyen6@gmail.com>
2023-03-04 14:32:56 -0600

    finalized DFM impl

--- musik.ino
+++ musik.ino
@@ -5,6 +5,7 @@ static const uint8_t PIN_DFM_RX = 3;
 static const uint8_t PIN_DFM_TX = 2;
 static const uint8_t PIN_TOGGLE_CONSTANT = 12;
 static const uint8_t PIN_TOGGLE_RANDOM = 13;
+static const uint8_t VOLUME_LEVEL = 30;
 
 SoftwareSerial softwareSerial(PIN_DFM_RX, PIN_DFM_TX);
 DFRobotDFPlayerMini player;
@@ -13,29 +14,23 @@ void setup() {
   Serial.begin(9600);
   softwareSerial.begin(9600);
 
+  // Initialize the toggle switch pins
   pinMode(PIN_TOGGLE_CONSTANT, INPUT_PULLUP);
   pinMode(PIN_TOGGLE_RANDOM, INPUT_PULLUP);
 
   if (player.begin(softwareSerial)) {
-    Serial.println("Initialized DFPlayer Mini");
-
-    // Set volume to maximum (0 to 30).
-    player.volume(30);
-    // Play   the first MP3 file on the SD card
-    player.play(1);
+    Serial.println("Initialized DFM!");
+    player.volume(VOLUME_LEVEL);
+    if (digitalRead(PIN_TOGGLE_CONSTANT) == HIGH) {
+      player.play(1); // First file will always be static ON file
+    } else if (digitalRead(PIN_TOGGLE_RANDOM) == HIGH) {
+      player.randomAll();
+    } else {
+      Serial.println("There was an issue wit the toggle switch!");
+    }
   } else {
-    Serial.println("Connecting   to DFPlayer Mini failed!");
+    Serial.println("There was an issue initializing DFM!");
   }
 }
 
-void loop() {
-  if (digitalRead(PIN_TOGGLE_CONSTANT) == HIGH) {
-      Serial.println("CONST HIGH");
-    }
-
-    if (digitalRead(PIN_TOGGLE_RANDOM) == HIGH) {
-      Serial.println("RAND HIGH");
-    }
-
-    
-}
+void loop() {}
